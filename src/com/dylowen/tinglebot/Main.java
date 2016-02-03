4f8cbb7c10852be5a789c99c967076dcb2118f03
Dylan Owen <dyltotheo@gmail.com>
2016-02-03 00:34:29 -0800

    First working version

--- src/com/dylowen/tinglebot/Main.java
+++ src/com/dylowen/tinglebot/Main.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
+import java.util.Random;
 
 /**
  * TODO add description
@@ -16,43 +17,73 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) {
-        Map<BiGram, String> dictionary = new HashMap<>();
+        Map<BiGram, WeightedSet<String>> dictionary = new HashMap<>();
 
         try {
             // FileReader reads text files in the default encoding.
-            FileReader fileReader = new FileReader("");
+            FileReader fileReader = new FileReader("/Users/dylan.owen/Desktop/tingchat.txt");
 
             // Always wrap FileReader in BufferedReader.
             BufferedReader bufferedReader = new BufferedReader(fileReader);
 
             String word;
 
-            int i = 0;
             LinkedList<String> words = new LinkedList<>();
             while ((word = readWord(bufferedReader)) != null) {
                 words.addLast(word);
 
-                System.out.println(word);
+                //System.out.println(word);
 
                 if (words.size() < 3) {
                     continue;
                 }
 
-                System.out.println(words.get(0) + " " + words.get(1) + " " + words.get(2));
-
                 final BiGram biGram = new BiGram(words.get(0), words.get(1));
-                if(dictionary.containsKey(biGram)) {
-                    System.out.println(biGram.toString() + " " + biGram.hashCode());
+                WeightedSet<String> set = dictionary.get(biGram);
+                if (set == null) {
+                    set = new WeightedSet<>();
+                    dictionary.put(biGram, set);
                 }
                 else {
-                    dictionary.put(biGram, words.get(2));
+                    //System.out.println(biGram.toString() + " [" + set.toString() + "]");
                 }
+                set.add(words.get(2));
 
                 words.removeFirst();
             }
 
+            System.out.println("Dictionary size: " + dictionary.size());
 
-            System.out.println(dictionary.size());
+
+            final Random rand = new Random();
+            //yeah this is the worst
+            @SuppressWarnings("unchecked")
+            Map.Entry<BiGram, WeightedSet<String>> first = (Map.Entry<BiGram, WeightedSet<String>>) dictionary.entrySet().toArray()[rand.nextInt(dictionary.size())];
+
+            LinkedList<String> sentence = new LinkedList<>();
+            sentence.add(first.getKey().one);
+            sentence.add(first.getKey().two);
+            sentence.add(first.getValue().get());
+            System.out.println(first.getKey().toString() + " [" + first.getValue().toString() + "]");
+
+            for (int i = 0; i < 20; i++) {
+                BiGram current = new BiGram(sentence.get(sentence.size() - 2), sentence.getLast());
+
+                WeightedSet<String> set = dictionary.get(current);
+                if (set != null) {
+                    System.out.println(current.toString() + " [" + set.toString() + "]");
+                    sentence.add(set.get());
+                }
+                else {
+                    break;
+                }
+            }
+
+            System.out.println();
+            for (String sentenceWord : sentence) {
+                System.out.print(sentenceWord + " ");
+            }
+            System.out.println();
 
         }
         catch (Exception e) {
@@ -61,6 +92,7 @@ public class Main {
     }
 
     //this is super crude
+    //TODO &apos;
     private static String readWord(final BufferedReader buffer)
             throws IOException {
         StringBuilder sb = new StringBuilder();
@@ -110,7 +142,7 @@ public class Main {
             return null;
         }
 
-        return sb.toString();
+        return sb.toString().toLowerCase();
     }
 
     private static class BiGram {
---
+++ src/com/dylowen/tinglebot/WeightedSet.java
@@ -0,0 +1,64 @@
+package com.dylowen.tinglebot;
+
+import java.util.Random;
+
+import gnu.trove.iterator.TObjectIntIterator;
+import gnu.trove.map.hash.TObjectIntHashMap;
+
+/**
+ * TODO add description
+ *
+ * @author dylan.owen
+ * @since Feb-2016
+ */
+public class WeightedSet<T> {
+    //trove map
+    final TObjectIntHashMap<T> map = new TObjectIntHashMap<>();
+    private int total = 0;
+
+    private Random rand = new Random();
+
+    public WeightedSet() {
+    }
+
+    public void add(final T obj) {
+        this.map.adjustOrPutValue(obj, 1, 1);
+
+        this.total++;
+    }
+
+    public T get() {
+        TObjectIntIterator<T> iterator = this.map.iterator();
+        int offset = 0;
+
+        for (int i = this.map.size(); i > 0; i--) {
+            iterator.advance();
+
+            final int rand = this.rand.nextInt(this.total) + 1;
+            offset += iterator.value();
+            if (rand <= offset) {
+                return iterator.key();
+            }
+        }
+
+        throw new AssertionError("we should never get here");
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder();
+
+        TObjectIntIterator<T> iterator = this.map.iterator();
+
+        for (int i = this.map.size(); i > 0; i--) {
+            iterator.advance();
+
+            sb.append(iterator.key());
+            sb.append(":");
+            sb.append(iterator.value());
+            sb.append(" ");
+        }
+
+        return sb.toString();
+    }
+}
